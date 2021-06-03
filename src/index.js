fa9565ad64c6cacd9f2317959154f5d4fd148527
AyoCodess <ayo.daniel.adesanya@gmail.com>
2021-06-03 14:08:28 +0200

    implemented javascript so we can change tabbed sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log(` wElCoMe`);
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+// select a tab content item
+
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  //add border to current tab
+  this.classList.add("tab-border");
+  console.log(`clicked`);
+
+  // add show class to content item
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+
+  tabContentItem.classList.remove("show");
+  tabContentItem.classList.add("show");
+}
+
+//remove border from current tabs
+function removeBorder() {
+  tabItems.forEach((item) => {
+    item.classList.remove("tab-border");
+  });
+}
+
+// Listen for a tab icon click
+tabItems.forEach((item) => {
+  item.addEventListener("click", selectItem);
+});
+
+//remove show class  from current tab
+function removeShow() {
+  tabContentItems.forEach((item) => {
+    item.classList.remove("show");
+  });
+}
